aa4af2e9ed833dc45f0071977b02897ea447765a
Jeff Hostetler <hostetjm@gmail.com>
2015-02-11 15:51:14 -0700

    deployment setup

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
+import ENV from '../config/environment';
+
+console.log(ENV.adapterURL)
 
 export default DS.ActiveModelAdapter.extend({
-  host: 'http://localhost:3000'
+  host: ENV.adapterURL
 });
